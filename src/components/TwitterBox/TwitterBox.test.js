1e85ed42eef86fa8e61c2eb58575e0b25fe570df
taverasmisael <macrot29@gmail.com>
2018-11-11 12:42:00 -0400

    feat: create base for the TwitterBox functionality

--- src/components/TwitterBox/TwitterBox.test.js
+++ src/components/TwitterBox/TwitterBox.test.js
@@ -1,11 +1,38 @@
 // @flow
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import TwitterBox from './TwitterBox'
 
 describe('component/TwitterBox', () => {
-  it('should match snapshot', () => {
-    const { container } = render(<TwitterBox />)
-    expect(container.firstChild).toMatchSnapshot()
+  const profile = {
+    name: 'Misael Taveras',
+    handler: '@taverasmisael',
+    url: 'https://twitter.com/taverasmisael',
+    picture:
+      'https://pbs.twimg.com/profile_images/926912434610114560/MAMgQ07A_bigger.jpg',
+  }
+
+  describe('core and non-open state', () => {
+    it('should match snapshot', () => {
+      const { container } = render(<TwitterBox />)
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should only display profile image and input when closed', () => {
+      const { getByAltText, getByLabelText } = render(<TwitterBox />)
+      expect(getByAltText('Profile')).toBeInTheDocument()
+      expect(getByLabelText('Write your tweet message')).toBeInTheDocument()
+    })
+
+    it('should render the right profile image', () => {
+      const { getByAltText } = render(<TwitterBox profile={profile} />)
+      expect(getByAltText('Profile').src).toBe(profile.picture)
+    })
+
+    it('should expand to a full version when the input has focus', () => {
+      const { getByLabelText } = render(<TwitterBox />)
+      const twitterBox = getByLabelText('Write your tweet message')
+      fireEvent.focus(twitterBox)
+    })
   })
 })
--- src/components/TwitterBox/Twitterbox.js
+++ src/components/TwitterBox/Twitterbox.js
@@ -1,6 +1,73 @@
 // @flow
-import React from 'react'
+import React, { PureComponent } from 'react'
+import classnames from 'classnames'
+import styles from './TwitterBox.module.sass'
 
-const TwitterBox = () => <div />
+type Props = {
+  profile: {
+    name: string,
+    handler: string,
+    url: string,
+    picture: string,
+  },
+}
+
+type State = {
+  isOpen: boolean,
+  canTweet: boolean,
+  tweetContent: string,
+}
+
+class TwitterBox extends PureComponent<Props, State> {
+  static defaultProps = {
+    profile: {},
+  }
+
+  state = {
+    isOpen: false,
+    canTweet: false,
+    tweetContent: '',
+  }
+  maxTweetLength = 10
+
+  render() {
+    const { profile } = this.props
+    const { isOpen, canTweet, tweetContent } = this.state
+    return (
+      <div
+        className={classnames(styles.container, {
+          [styles.containerIsOpen]: isOpen,
+        })}
+      >
+        <img
+          alt="Profile"
+          src={profile.picture}
+          className={styles.profilePic}
+        />
+        <textarea
+          type="text"
+          aria-label="Write your tweet message"
+          rows="1"
+          data-gramm="false"
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          className={styles.textarea}
+        />
+        <div className={styles.actions}>
+          <button
+            disabled={!canTweet}
+            type="button"
+            className={`button is-primary is-rounded ${styles.submit}`}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  onFocus = () => this.setState({ isOpen: true })
+  onBlur = () => this.setState({ isOpen: false })
+}
 
 export default TwitterBox
