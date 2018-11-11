1c4b3c67bc09249575230fc2b6e2a12bb4d09aca
taverasmisael <macrot29@gmail.com>
2018-11-11 12:37:42 -0400

    feat: add mock profile to pass to TwitterBox

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,10 +2,18 @@
 import React from 'react'
 import TwitterBox from '../../components/TwitterBox'
 
+const profile = {
+  name: 'Misael Taveras',
+  handler: '@taverasmisael',
+  url: 'https://twitter.com/taverasmisael',
+  picture:
+    'https://pbs.twimg.com/profile_images/926912434610114560/MAMgQ07A_bigger.jpg',
+}
+
 const App = () => (
   <>
     <h1>Twitter Box Example</h1>
-    <TwitterBox />
+    <TwitterBox profile={profile} />
   </>
 )
 
